0ba4a8c6cd9284b2f48598ee9dffead45faa18dc
webcode <erlang.ug@gmail.com>
2012-11-20 11:06:18 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nuapp2::Application.config.secret_token = '53e7e6b9f82312b52b206dccc6fd22f61ca22056f1efda44bc5e68af79c65a89bd4d02170ad512b8341d9d6047b980de8912742bb6fd87be17ebb204fe6357c6'
