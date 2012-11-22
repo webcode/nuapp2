0c07f510a128ff0e8e92a072dcc3a9d631c3e083
webcode <erlang.ug@gmail.com>
2012-11-23 00:51:36 +0300

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+describe "Static pages" do
+  describe "Home page" do
+    it "should have the content 'nuapp2'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'nuapp2')
+    end
+    it "should have the right title" do
+  visit '/static_pages/home'
+  page.should have_selector('title',
+                    :text => "nuapp2 by webber of jilinsights | Home")
+end
+
+  end
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+     it "should have the right title" do
+  visit '/static_pages/help'
+  page.should have_selector('title',
+                    :text => "nuapp2 by webber of jilinsights | Help")
+end
+
+  end
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+     it "should have the right title" do
+  visit '/static_pages/about'
+  page.should have_selector('title',
+                    :text => "nuapp2 by webber of jilinsights | About Us")
+end
+
+  end
+end
+
