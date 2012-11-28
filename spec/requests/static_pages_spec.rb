73495a8b66cb97fb368a2d442e34b7cc021058ce
webcode <erlang.ug@gmail.com>
2012-11-28 14:34:47 +0300

    Message

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,27 +2,27 @@ require 'spec_helper'
 describe "Static pages" do
   describe "Home page" do
     it "should have the content 'nuapp2'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('h1', :text => 'nuapp2')
     end
     it "should have the right title" do
-  visit '/static_pages/home'
+  visit root_path
   page.should have_selector('title',
                     :text => "nuapp2 by chriso")
 end
   it "should not have a custom page title" do
-    visit '/static_pages/home'
+    visit root_path
     page.should_not have_selector('title', :text => '| Home')
   end
 
   end
   describe "Help page" do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('h1', :text => 'Help')
     end
      it "should have the right title" do
-  visit '/static_pages/help'
+  visit help_path
   page.should have_selector('title',
                     :text => "nuapp2 by chriso | Help")
 end
@@ -30,15 +30,26 @@ end
   end
   describe "About page" do
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
+      visit about_path
+      page.should have_selector('h1', text: 'About Us')
     end
      it "should have the right title" do
-  visit '/static_pages/about'
+  visit about_path
   page.should have_selector('title',
-                    :text => "nuapp2 by chriso | About Us")
+                    text: "nuapp2 by chriso | About Us")
+ end
 end
-
+describe "Contact page" do
+  it "should have the h1 'Contact'" do
+    visit contact_path
+    page.should have_selector('h1', :text => 'Contact')
   end
+  it "should have the title 'Contact'" do
+    visit contact_path
+    page.should have_selector('title',
+                  :text => "nuapp2 by chriso | Contact")
+  end
+
+ end
 end
 
