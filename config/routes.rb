1fc3d82f5765bdc22c1e86539237f8c3f7c4554f
webcode <erlang.ug@gmail.com>
2012-12-05 02:59:39 +0300

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Nuapp2::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   root to: "static_pages#home"
 
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+     name	'webz'
+     email	'musinguzi.christian@gmail.com'
+     password 'webzo1234'
+     password_confirmation 'webzo1234'
+  end
+end
